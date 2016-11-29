6a7fe50a56480488f40c1bb656c225f5dd7b7809
carmenicuadrado <carmenicuadrado@gmail.com>
2016-11-29 11:24:27 +0100

    cambio carmen

--- HolaMundo.java
+++ HolaMundo.java
@@ -3,7 +3,7 @@ package pruebas;
 public class HolaMundo {
 
 	public static void main(String[] args) {
-		System.out.println("Hola" + "\"" + args[0]);
+		System.out.println("Hola" + "\"" + args[0] + arg[1]);
 		System.out.println(true);
 		System.out.println();
 
