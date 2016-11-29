ae074ae172c3737bcc763480630ae3ca14b76f55
Jopau3112 <jopau3112@gmail.com>
2016-11-29 11:21:38 +0100

    Cambio hecho por Jopau3112

--- HolaMundo.java
+++ HolaMundo.java
@@ -3,7 +3,7 @@ package pruebas;
 public class HolaMundo {
 
 	public static void main(String[] args) {
-		System.out.println("Hola" + "\"" + args[0]);
+		System.out.println("Hola de nuevo");
 		System.out.println(true);
 		System.out.println();
 
